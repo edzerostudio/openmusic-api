c983270a273f4b92622a69f4a65c8ab415cdc6d4
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2022-10-18 01:07:35 +0700

    Update Core

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,9 @@
+
+const authentications = require('./authentications');
+const users = require('./users');
+const collaborations = require('./collaborations');
+const playlists = require('./playlists');
 const albums = require('./albums');
 const songs = require('./songs');
 
-module.exports = { albums, songs };
+module.exports = { authentications, users, collaborations, playlists, albums, songs };
--- src/services/postgres/index.js
+++ src/services/postgres/index.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
+const AuthenticationsService = require('./AuthenticationsService');
+const UsersService = require('./UsersService');
+const CollaborationsService = require('./CollaborationsService');
+const PlaylistsService = require('./PlaylistsService');
 const AlbumsService = require('./AlbumsService');
 const SongsService = require('./SongsService');
 
 class StorageService {
   constructor() {
-    this.pool = new Pool();
+    this._pool = new Pool();
 
+    this.authenticationsService = new AuthenticationsService(this);
+    this.usersService = new UsersService(this);
+    this.collaborationsService = new CollaborationsService(this);
+    this.playlistsService = new PlaylistsService(this);
     this.albumsService = new AlbumsService(this);
     this.songsService = new SongsService(this);
   }
