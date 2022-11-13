55b3d775fb6d3bbfb8e4e778e6ee70997a8ccc71
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2022-11-13 21:28:53 +0700

    Update Index

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,12 @@
-
 const authentications = require('./authentications');
 const users = require('./users');
 const collaborations = require('./collaborations');
 const playlists = require('./playlists');
 const albums = require('./albums');
 const songs = require('./songs');
+const _exports = require('./exports');
+const uploads = require('./uploads');
 
-module.exports = { authentications, users, collaborations, playlists, albums, songs };
+module.exports = {
+  authentications, users, collaborations, playlists, albums, songs, _exports, uploads,
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,38 @@
+const path = require('path');
 const PostgresService = require('./postgres');
+const LocalStorageService = require('./storage/StorageService');
+const S3StorageService = require('./S3/StorageService');
+const ProducerService = require('./rabbitmq/ProducerService');
+const config = require('../utils/config');
 
-module.exports = new PostgresService();
+const {
+  authenticationsService,
+  usersService,
+  collaborationsService,
+  playlistsService,
+  albumsService,
+  songsService,
+} = new PostgresService();
+
+let storageService = new LocalStorageService(path.resolve(__dirname, '../api/uploads/file/images'));
+switch (config.app.storage) {
+  case config.storage.s3:
+    storageService = new S3StorageService();
+    break;
+  case config.storage.local:
+  default:
+    storageService = new LocalStorageService(path.resolve(__dirname, '../api/uploads/file/images'));
+    break;
+}
+const producerService = ProducerService;
+
+module.exports = {
+  authenticationsService,
+  usersService,
+  collaborationsService,
+  playlistsService,
+  albumsService,
+  songsService,
+  storageService,
+  producerService,
+};
